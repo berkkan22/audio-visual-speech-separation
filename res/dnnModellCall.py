99b0ee31f26ca2b57516edd0bfb1fef109b3e42b
studio <studio@informatik.uni-hamburg.de>
2023-02-02 13:42:42 +0100

    after 20 new audio frames are road in the DNN call will be started

--- res/dnnModellCall.py
+++ res/dnnModellCall.py
@@ -12,23 +12,27 @@ class DnnModelCall(Process):
 
     def run(self):
         print("DnnModelCall: run")
-        # localBuffer = [0] * 40800
-        localBuffer = []
-        # count = 0
+        localBuffer = [0] * 40800
+        # localBuffer = []
+        count = 0
         while True:
             if(not self.audioBufferInQueueParam.empty()):
 
-                while(not self.audioBufferInQueueParam.empty() and len(localBuffer) <= 40800):
-                    # localBuffer = removeFirstFrameAndAddNewFrame(localBuffer, self.audioBufferInQueueParam.get())
-                    localBuffer.extend(self.audioBufferInQueueParam.get())
+                # while(not self.audioBufferInQueueParam.empty() and len(localBuffer) <= 40800):
+                while(not self.audioBufferInQueueParam.empty() and count < 20):
+                    localBuffer = removeFirstFrameAndAddNewFrame(localBuffer, self.audioBufferInQueueParam.get())
+                    # localBuffer.extend(self.audioBufferInQueueParam.get())
                     # print(f"localBuffer: {len(localBuffer)}")
+                    count += 1
 
-            if(len(localBuffer) >= 40800):
+            # if(len(localBuffer) >= 40800):
+            if(count >= 20):
                 print("Start")
                 audioBuffer = localBuffer
                 print(len(localBuffer))
                 # print(len(audioBuffer[-2560::]))
-                time.sleep(0.16)
-                self.audioBufferDNNOutParam.put(audioBuffer)
-                localBuffer = []
+                time.sleep(0.1) # laufzeit von DNN
+                self.audioBufferDNNOutParam.put(audioBuffer[-2560::])
+                # localBuffer = []
+                count = 0
                 
